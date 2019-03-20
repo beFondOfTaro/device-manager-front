a258c0f20f29e9a65be1ce4866871be7798277e5
hyz <609927332@qq.com>
2019-03-20 23:18:31 +0800

    feat:完成用户表格行颜色，分页支持

--- src/views/system-setting/user-management/components/UserTable.vue
+++ src/views/system-setting/user-management/components/UserTable.vue
@@ -1,48 +1,57 @@
 <template>
-  <el-table
-    :data="userList"
-    :row-class-name="tableRowClassName"
-    style="width: 100%">
-    <el-table-column
-      prop="id"
-      label="id"
-      width="180"/>
-    <el-table-column
-      prop="username"
-      label="用户名"
-      width="180"/>
-    <el-table-column
-      prop="realName"
-      label="姓名"
-      width="180"/>
-    <el-table-column
-      prop="phone"
-      label="手机号"/>
-    <el-table-column
-      prop="email"
-      label="邮箱"/>
-    <el-table-column
-      label="角色">
-      <template slot-scope="scope">
-        {{ scope.row.roleList | formatRoles }}
-      </template>
-    </el-table-column>
-    <el-table-column
-      label="创建时间">
-      <template slot-scope="scope">
-        {{ scope.row.createTime | formatCreateTime }}
-      </template>
-    </el-table-column>
-    <el-table-column
-      fixed="right"
-      label="操作"
-      width="100">
-      <template slot-scope="scope">
-        <el-button type="text" size="small">查看</el-button>
-        <el-button type="text" size="small">编辑</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div>
+    <el-table
+      :data="userList"
+      :row-class-name="tableRowClassName"
+      style="width: 100%">
+      <el-table-column
+        prop="id"
+        label="id"
+        width="180"/>
+      <el-table-column
+        prop="username"
+        label="用户名"/>
+      <el-table-column
+        prop="realName"
+        label="姓名"/>
+      <el-table-column
+        prop="phone"
+        label="手机号"/>
+      <el-table-column
+        prop="email"
+        label="邮箱"/>
+      <el-table-column
+        label="角色">
+        <template slot-scope="scope">
+          {{ scope.row.roleList | formatRoles }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="创建时间">
+        <template slot-scope="scope">
+          {{ scope.row.createTime | formatCreateTime }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="180">
+        <template slot-scope="scope">
+          <el-button type="text" size="small">查看</el-button>
+          <el-button type="text" size="small">编辑</el-button>
+          <el-button type="text" size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      :current-page="listUserRequest.queryPage.pageNum"
+      :page-sizes="[10, 50, 100, 200]"
+      :page-size="listUserRequest.queryPage.pageSize"
+      :total="totalCount"
+      layout="total, sizes, prev, pager, next, jumper"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"/>
+  </div>
 </template>
 
 <script>
@@ -69,7 +78,9 @@ export default {
       listUserRequest: {
         queryPage: new QueryPage()
       },
-      userList: []
+      userList: [],
+      // 用户总数
+      totalCount: 0
     }
   },
   mounted() {
@@ -79,26 +90,38 @@ export default {
     listUser() {
       listUser(this.listUserRequest).then(res => {
         this.userList = res.data.list
+        this.totalCount = res.data.total
       })
     },
+    // 表格行的css class
     tableRowClassName({ row, rowIndex }) {
-      if (rowIndex === 1) {
-        return 'warning-row'
-      } else if (rowIndex === 3) {
-        return 'success-row'
+      for (let role of row.roleList) {
+        if (role.id === '1') {
+          return 'sys-admin-row'
+        }
       }
       return ''
+    },
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`)
+      this.listUserRequest.queryPage.pageSize = val
+      this.listUser()
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`)
+      this.listUserRequest.queryPage.pageNum = val
+      this.listUser()
     }
   }
 }
 </script>
 
-<style scoped>
-  .el-table .warning-row {
+<style>
+  .el-table .sys-admin-row {
     background: oldlace;
   }
 
-  .el-table .success-row {
+  .el-table .normal-row {
     background: #f0f9eb;
   }
 </style>
